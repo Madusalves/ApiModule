a99e68f61f839275a19663661e4f02d57823994e
Maria Eduarda <malvesduarte444@gmail.com>
2023-12-14 14:14:20 -0300

    commit

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiModule.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new{
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortDateString()
+            };
+            return Ok(obj);
+        }
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var mensagem = $"Ola {nome}, seja bem vindo";
+            return Ok(new{mensagem});
+        }
+    }
+}
